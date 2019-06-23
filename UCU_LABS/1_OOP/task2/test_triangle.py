30a69eb4c80aa32e2595d5c6679f02da4eefb25f
sophiashuv <45092554+sophiashuv@users.noreply.github.com>
2019-06-23 13:28:54 +0300

    Delete test_triangle.py

--- UCU_LABS/1_OOP/task2/test_triangle.py
+++
@@ -1,8 +0,0 @@
-import point
-from triangle import Triangle
-
-
-triangle = Triangle(point.Point(1,1), point.Point(3,1), point.Point(2,3))
-print(triangle.is_triangle())
-print(triangle.perimeter())
-print(triangle.area())
